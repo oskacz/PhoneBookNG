6e9564c5a0f09e08bc959702d96040d1dfded364
Radosław Frankowski <r.franek93@gmail.com>
2020-11-04 18:40:11 +0100

    dodanie filtrow

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -2,20 +2,40 @@ import { Component, Input, OnInit } from '@angular/core';
 import { PhonebookserviceService } from '../core/phonebookservice.service';
 import { Contacts } from '../services/model';
 import { MatTableDataSource } from '@angular/material/table';
+import {AfterViewInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit , AfterViewInit {
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private serv: PhonebookserviceService) { }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
   contacts: Contacts[];
   contactToDeleteId: number;
   displayedColumns: string[] = ['Imie', 'Nazwisko', 'Telefon', 'Adres' ,'Miasto', 'Kod pocztowy','Edytuj','Usun'];
   dataSource: MatTableDataSource<Contacts>;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
   rerender(): void {
     this.dataSource = new MatTableDataSource(this.contacts);
